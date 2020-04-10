e0356db909395e02da980d1727c5841baf1334e9
ankit <ankit.sharma2@go-mmt.com>
2020-04-10 19:30:26 +0530

    updated folder structure

---
+++ LRU/DoublyLinkListNode.java
@@ -0,0 +1,12 @@
+public class DoublyLinkListNode{
+    
+    int key;
+    int value;
+    DoublyLinkListNode prev;
+    DoublyLinkListNode next;
+    
+    DoublyLinkListNode(int key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
